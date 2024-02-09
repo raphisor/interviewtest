7dd54579784eb0dadf453f63d2e539597255367c
Gabor Strenger <strenger.gabor@gmail.com>
2024-02-09 09:06:38 +0100

    Add new selenium grid compose

--- src/main/java/com/strenger/interviewtest/config/RemoteWebDriverConfig.java
+++ src/main/java/com/strenger/interviewtest/config/RemoteWebDriverConfig.java
@@ -4,6 +4,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -22,8 +24,25 @@ public class RemoteWebDriverConfig {
   @ConditionalOnMissingBean
   @ConditionalOnProperty(name = "browser", havingValue = "chrome")
   @Primary
-  public WebDriver remoteChromeDriver() throws MalformedURLException {
+  public WebDriver chromeDriver() throws MalformedURLException {
     ChromeOptions chromeOptions = new ChromeOptions();
     return new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
   }
+
+  @Bean
+  @ConditionalOnProperty(name = "browser", havingValue = "edge")
+  public WebDriver edgeDriver() throws MalformedURLException {
+    EdgeOptions edgeOptions = new EdgeOptions();
+    return new RemoteWebDriver(new URL("http://localhost:4444"), edgeOptions);
+  }
+
+  @Bean
+  @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+  @Primary
+  public WebDriver firefoxDriver() throws MalformedURLException {
+    FirefoxOptions chromeOptions = new FirefoxOptions();
+    return new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+  }
+
+
 }
--- src/main/java/com/strenger/interviewtest/config/WebDriverConfig.java
+++ src/main/java/com/strenger/interviewtest/config/WebDriverConfig.java
@@ -3,19 +3,11 @@ package com.strenger.interviewtest.config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 
-@Profile("!grid")
-@Lazy
-@Configuration
+
 public class WebDriverConfig {
 
-  @Bean
-  @Primary
+
   public WebDriver webDriver() {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
